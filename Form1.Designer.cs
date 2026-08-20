7c8b983b4f83dd9a33e5d73b5ba813b998a8d45a
JoshuaOlguin <joshua.e.olguin@gmail.com>
2026-08-19 23:27:29 -0600

    Windows Form  Application completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmbProvider = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtBoxSearchParameter = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnGenerateFile = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -49,13 +49,13 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Proveedor:";
             // 
-            // comboBox1
+            // cmbProvider
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(113, 67);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(295, 28);
-            this.comboBox1.TabIndex = 1;
+            this.cmbProvider.FormattingEnabled = true;
+            this.cmbProvider.Location = new System.Drawing.Point(113, 67);
+            this.cmbProvider.Name = "cmbProvider";
+            this.cmbProvider.Size = new System.Drawing.Size(295, 28);
+            this.cmbProvider.TabIndex = 1;
             // 
             // label2
             // 
@@ -66,12 +66,12 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "2.- Introduce el nombre del articulo que deseas buscar:";
             // 
-            // textBox1
+            // txtBoxSearchParameter
             // 
-            this.textBox1.Location = new System.Drawing.Point(113, 159);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(295, 26);
-            this.textBox1.TabIndex = 3;
+            this.txtBoxSearchParameter.Location = new System.Drawing.Point(113, 159);
+            this.txtBoxSearchParameter.Name = "txtBoxSearchParameter";
+            this.txtBoxSearchParameter.Size = new System.Drawing.Size(295, 26);
+            this.txtBoxSearchParameter.TabIndex = 3;
             // 
             // label3
             // 
@@ -92,49 +92,52 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Articulo:";
             // 
-            // button1
+            // btnSearch
             // 
-            this.button1.Location = new System.Drawing.Point(20, 226);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 47);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSearch.Location = new System.Drawing.Point(20, 226);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 47);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
-            // button2
+            // btnGenerateFile
             // 
-            this.button2.Location = new System.Drawing.Point(163, 226);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 47);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnGenerateFile.Location = new System.Drawing.Point(139, 226);
+            this.btnGenerateFile.Name = "btnGenerateFile";
+            this.btnGenerateFile.Size = new System.Drawing.Size(151, 47);
+            this.btnGenerateFile.TabIndex = 7;
+            this.btnGenerateFile.Text = "Generar Archivo";
+            this.btnGenerateFile.UseVisualStyleBackColor = true;
+            this.btnGenerateFile.Click += new System.EventHandler(this.btnGenerateFile_Click);
             // 
-            // button3
+            // btnReset
             // 
-            this.button3.Location = new System.Drawing.Point(333, 226);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 47);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnReset.Location = new System.Drawing.Point(333, 226);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 47);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Limpiar";
+            this.btnReset.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(437, 302);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnGenerateFile);
+            this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtBoxSearchParameter);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmbProvider);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,14 +146,14 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbProvider;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtBoxSearchParameter;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnGenerateFile;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 using System.Windows.Forms;
+using WinFormProviderCatalogReader.Dto;
+using WinFormProviderCatalogReader.Utilities;
 
 namespace WinFormProviderCatalogReader
 {
     public partial class Form1 : Form
     {
+
+        private string apiKey = "7x7X2W9L3A";
+        private int maxResults;
+        private ProductResponse productResponse;
+        private List<Item> items;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbProvider.Items.Add("Fixoem");
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtBoxSearchParameter.Text))
+            {
+                ApiClientService client = new ApiClientService();
+
+                try
+                {
+                    string apiResponse = await client.GetCatalogBySearchParameter(apiKey, Common.HtmlEncodeStringValue(txtBoxSearchParameter.Text));
+                    JsonDocument document = JsonDocument.Parse(apiResponse);
+                    maxResults = document.RootElement.GetProperty("totalItems").GetInt32();
+
+                    string result = await client.GetCatalogBySearchParameter(apiKey, Common.HtmlEncodeStringValue(txtBoxSearchParameter.Text), maxResults);
+                    productResponse = Common.DeserializeResponse<ProductResponse>(result);
+
+                    items = Adapter.ConvertProductResponseItemsToItemList(productResponse);
+                    MessageBox.Show("Obtención de catalogo del provedor completada.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, introduce el nombre del artículo que deseas buscar.", "Error Operacion invalida.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGenerateFile_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Title = "Select a folder";
+                saveFileDialog.Filter = "All files (*.*)|*.*";
+                saveFileDialog.FileName = $"{txtBoxSearchParameter.Text}.xlsx";
+                saveFileDialog.CheckPathExists = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    Excel.CreateFile(filePath);
+                    Excel.PopulateExcelFile(filePath, items);
+
+                    MessageBox.Show("Archivo excel generado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
         }
     }
 }
